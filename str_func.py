c802ca8e199a900367ea4512e7c0d705341deff4
Alexsey Gladkikh <alexseygladkikh@mail.ru>
2024-10-30 15:39:53 +0300

    new func and docstring

--- str_func.py
+++ str_func.py
@@ -1,4 +1,12 @@
 def str_up(text):
+    """Функция выводит весь текст с заглавной буквы"""
     return text.upper()
+
+print(str_up("hello world"))
+
+
+def str_up(text):
+    """Эта функция делает заглавными первые буквы"""
+    return text.title()
 
 print(str_up("hello world"))
